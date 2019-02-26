dd1d079cd237f91713b7c072fd9af97d158e2de2
John <johnmcguin88@gmail.com>
2019-02-26 09:30:59 -0700

    add more cities, add favicon

--- services/craigslist.service.js
+++ services/craigslist.service.js
@@ -11,7 +11,14 @@ async function getSkooliesCL() {
         'https://seattle.craigslist.org/search/sss?format=rss&query=skoolie&sort=rel',
         'https://portland.craigslist.org/search/sss?format=rss&query=skoolie&sort=rel',
         'https://santafe.craigslist.org/search/sss?format=rss&query=skoolie&sort=rel',
-        'https://austin.craigslist.org/search/sss?format=rss&query=skoolie&sort=rel'
+        'https://austin.craigslist.org/search/sss?format=rss&query=skoolie&sort=rel',
+        'https://chattanooga.craigslist.org/search/sss?format=rss&query=skoolie&sort=rel',
+        'https://bend.craigslist.org/search/sss?format=rss&query=skoolie&sort=rel',
+        'https://sfbay.craigslist.org/search/sss?format=rss&query=skoolie&sort=rel',
+        'https://missoula.craigslist.org/search/sss?format=rss&query=skoolie&sort=rel',
+        'https://rockies.craigslist.org/search/sss?format=rss&query=skoolie&sort=rel',
+        'https://westslope.craigslist.org/search/sss?format=rss&query=skoolie&sort=rel',
+        'https://asheville.craigslist.org/search/sss?format=rss&query=skoolie&sort=rel'
     ];
 
     const promises = CLCities.map(city => axios.get(city));
